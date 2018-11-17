7b56373225272461bf6cc102661b5488ccc56095
G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>
2018-11-18 00:02:45 +0530

    Adds .browserslistrc .editorconfig .eslintrc.js
    Signed-off-by: G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './stores'
+import * as filters from './filters' // global filters
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
