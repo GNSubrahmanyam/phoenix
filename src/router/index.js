6492197e4fedaa213e1e70a8ccc281d8daf4e45a
G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>
2018-11-26 15:22:37 +0530

    Uses alias
    Signed-off-by: G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import VueMeta from 'vue-meta'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
+import Home from '@views/Home.vue'
+import About from '@views/About.vue'
 
 Vue.use(Router)
 Vue.use(VueMeta, {
