d7012a7a7eda73b4c399c9a14ce42e6d7b32f0f7
G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>
2018-11-26 15:20:44 +0530

    Adds modular store
    Signed-off-by: G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Modules
+import modules from './modules'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  getters: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  modules,
+  // Enable strict mode in development to get a warning
+  // when mutating state outside of a mutation.
+  // https://vuex.vuejs.org/guide/strict.html
+  strict: process.env.NODE_ENV !== 'production'
 })
