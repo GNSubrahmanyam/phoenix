609349f8a372af9a2ce08f30a7f637b7303fe91e
G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>
2018-11-26 15:17:52 +0530

    - Uses aliases
    Signed-off-by: G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
-import router from './router'
-import store from './stores'
-import * as filters from './filters' // global filters
+import router from '@router'
+import store from '@stores'
+import * as filters from '@filters' // global filters
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 // register global utility filters.
 Object.keys(filters).forEach(key => {
