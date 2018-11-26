dbafffadb295809d441312586a19bcf2efe34a30
G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>
2018-11-26 15:21:27 +0530

    Adds aliases config file
    Signed-off-by: G Naga Subrahmanyam <gnsubrahmanyam@doselect.com>

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,12 @@ module.exports = {
   configureWebpack: {
     // We provide the app's title in Webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
-    name: appConfig.title
+    name: appConfig.title,
+    // Set up all the aliases we use in our app.
+    resolve: {
+      alias: require('./aliases.config').webpack
+    },
+    devtool: 'source-map'
   },
   css: {
     // Enable CSS source maps.
